7867ae753785acf21033965dfc8e991e82d10039
Martijn Braam <martijn@brixit.nl>
2017-06-05 22:11:20 +0200

    Mainline hammerhead improvements

--- aports/device-lg-hammerhead/initfs-hook.sh
+++ aports/device-lg-hammerhead/initfs-hook.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
 
+
+echo "\n\n\n\n\n"
+blkid
+echo "\n\n\n"
+
+getty -L /bin/console 0 vt102
+
+
 # set framebuffer resolution
 cat /sys/class/graphics/fb0/modes > /sys/class/graphics/fb0/mode
